10ea7be88bf86aceabcf265515891e37ac4cf2cc
Giovanni de la Rosa <giovadelarosa@gmail.com>
2021-09-27 19:17:34 -0400

    add my reviung34 keymap

---
+++ keyboards/reviung34/keymaps/default_rgb_gio/keymap.c
@@ -0,0 +1,73 @@
+/* Copyright 2021 gtips
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum layer_names {
+  _BASE,
+  _LOWER,
+  _RAISE,
+  _ADJUST
+};
+
+#define LOWER  MO(_LOWER)
+#define RAISE  MO(_RAISE)
+
+#define CT_Z  LCTL_T(KC_Z)
+#define CT_BSLS  LCTL_T(KC_BSLS)
+#define CT_Z  LCTL_T(KC_Z)
+#define CT_QUOT RCTL_T(KC_QUOT)
+#define GUI_ESC LGUI_T(KC_ESC)
+#define AL_X  LALT_T(KC_X)
+#define AL_DT RALT_T(KC_DOT)
+#define AL_LBRC RALT_T(KC_LBRC)
+#define AL_ESC RALT_T(KC_ESC)
+#define LO_TAB LT(LOWER, KC_TAB)
+#define RA_SPC LT(RAISE, KC_SPC)
+#define SF_BSPC RSFT_T(KC_BSPC)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT_reviung34(
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,          KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
+    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,          KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
+    CT_Z,    AL_X,    KC_C,    KC_D,    KC_V,          KC_K,    KC_H,    KC_COMMA,   AL_DT,   CT_QUOT,
+                        SF_BSPC,  GUI_ESC,          LO_TAB,   RA_SPC
+  ),
+
+  [_LOWER] = LAYOUT_reviung34(
+    KC_F9, KC_F10, KC_F11, KC_F12, XXXXXXX,            KC_PLUS, KC_7, KC_8, KC_9, KC_ASTR,
+    KC_F5, KC_F6,  KC_F7, KC_F8, XXXXXXX,              KC_MINS, KC_4, KC_5,  KC_6, KC_0,
+    KC_F1, KC_F2,  KC_F3, KC_F4, XXXXXXX,              KC_SLSH, KC_1, KC_2, KC_3, KC_EQL,
+                        _______, _______,              _______, _______
+  ),
+
+  [_RAISE] = LAYOUT_reviung34(
+    KC_QUES, KC_LPRN, KC_RPRN, KC_PIPE, XXXXXXX,        KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX,
+    KC_SLSH, KC_LCBR, KC_RCBR, KC_MINS, KC_UNDS,        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
+    CT_BSLS, AL_LBRC,  KC_RBRC,  KC_GRV,  KC_TILD,        XXXXXXX,  XXXXXXX, XXXXXXX,  KC_RALT,  KC_RCTL,
+                          KC_ENT, KC_DEL,        _______, _______
+  ),
+  
+  [_ADJUST] = LAYOUT_reviung34(
+    RGB_VAI, RGB_SAI, RGB_HUI, RGB_MOD, RGB_TOG,       KC_BRIU,   KC_VOLU,   KC_MPLY,   KC_MNXT,   XXXXXXX,  
+    RGB_VAD, RGB_SAD, RGB_HUD, RGB_RMOD,XXXXXXX,       KC_BRID,   KC_VOLD,   KC_MPRV,   XXXXXXX,   XXXXXXX, 
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,         XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
+                              _______, _______,       _______, _______
+  ),
+};
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
